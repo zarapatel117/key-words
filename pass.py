3e6fe86c912db8eb5822a6201e3d8ae357a12113
zarapatel117 <zarapatel2510@gmail.com~>
2025-01-09 20:26:12 +0530

    pass

---
+++ pass.py
@@ -0,0 +1,12 @@
+for x in range(10):
+    if x%20==0:
+        print("hello")
+    elif x%15==0:
+        pass
+    elif x%10==0:
+        print("hi")
+    elif x%5==0:
+        print("blah") 
+    else:
+        print("nothing")       
+
